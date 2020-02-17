02214eb4c4d47581e5587d79aadb75a24744a42f
travis-john <travis.wl.john@gmail.com>
2020-02-17 10:43:00 -0500

    removing getMovies function - query happens on page load

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,26 +18,22 @@ for ( let i = 0; i < $starCount; i++){
   });
 }
 
-function getMovies() {
-  $.ajax({
-    url: $query,
-    method: 'GET'
-  }).then(function(response) {
-    console.log(response);
-    for (let i = 0; i < response.results.length; i++) {
-      $('.movie-title-row').append(`
-          <div class='col-12 col-sm-6 col-md-4'>
-            <div class ='film'>
-              <h2 class='film-title'>${response.results[i].title.toLowerCase()}</h2>
-              <div class='crawl d-none'>
-                <h2 class='crawl-title'>${response.results[i].title}</h2>
-                <p class='crawl-text'>${response.results[i].opening_crawl}</p>
-              </div>
+$.ajax({
+  url: $query,
+  method: 'GET'
+}).then(function(response) {
+  console.log(response);
+  for (let i = 0; i < response.results.length; i++) {
+    $('.movie-title-row').append(`
+        <div class='col-12 col-sm-6 col-md-4'>
+          <div class ='film'>
+            <h2 class='film-title'>${response.results[i].title.toLowerCase()}</h2>
+            <div class='crawl d-none'>
+              <h2 class='crawl-title'>${response.results[i].title}</h2>
+              <p class='crawl-text'>${response.results[i].opening_crawl}</p>
             </div>
           </div>
-        `)
-    }
-  });
-}
-
-getMovies();
+        </div>
+      `)
+  }
+});
