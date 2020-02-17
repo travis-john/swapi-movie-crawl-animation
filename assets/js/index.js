b1f6affa331c51fd90b9759f87cb5b2a26775d6a
travis-john <travis.wl.john@gmail.com>
2020-02-17 11:41:53 -0500

    pushing click event for debugging

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,18 +23,26 @@ $.ajax({
   method: 'GET'
 }).then(function(response) {
   console.log(response);
-  films = response.results;
+  let films = response.results;
   for (let i = 0; i < films.length; i++) {
     $('.movie-title-row').append(`
         <div class='col-12 col-sm-6 col-md-4'>
           <div class ='film'>
             <h2 class='film-title'>${films[i].title.toLowerCase()}</h2>
             <div class='crawl d-none'>
+              <div class='logo'>S</div>
               <h2 class='crawl-title'>${films[i].title}</h2>
               <p class='crawl-text'>${films[i].opening_crawl}</p>
             </div>
           </div>
         </div>
-      `)
+      `);
   }
+});
+
+$('.film').on('click', function() {
+
+  console.log('clicked');
+
+  $(this).find('.crawl').toggleClass('d-none');
 });
