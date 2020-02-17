a79a83dded03d97e0e1627e150cbb0b1278d2584
travis-john <travis.wl.john@gmail.com>
2020-02-17 10:01:18 -0500

    api call function - SWAPI down

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,26 @@
 let $parent = $('body'),
     $starCount = 250,
     $parentHeight = $parent.height(),
-    $parentWidth = $parent.width();
+    $parentWidth = $parent.width(),
+    $query = 'https://swapi.co/api/films';
 
-    for (i=0; i<$starCount; i++){
-        var $randPosX = Math.floor((Math.random()*$parentWidth)),
-            $randPosY = Math.floor((Math.random()*$parentHeight));
-        // console.log($randPosY);
-        $parent.append('<div class="star star--position-'+i+'"></div>');
+for ( let i = 0; i < $starCount; i++){
 
-        $('.star--position-'+i).each(function(){
-        $(this).css({
-          left: $randPosX,
-          top: $randPosY
-        });
-      });
-    }
+  let $randPosX = Math.floor((Math.random()*$parentWidth)),
+      $randPosY = Math.floor((Math.random()*$parentHeight));
+      $parent.append('<div class="star star--position-'+i+'"></div>');
+
+   $('.star--position-'+i).each(function(){
+     $(this).css({
+     left: $randPosX,
+     top: $randPosY
+    });
+  });
+}
+
+$.ajax({
+  url: $query,
+  method: 'GET'
+}).then(function(response) {
+  console.log(response);
+});
